857e5508e91435b4f20c8685ad85762626818029
zmiyov <zmiyov@i.ua>
2020-04-02 15:51:54 +0300

    added template

--- client/src/components/Root.js
+++ client/src/components/Root.js
@@ -1,9 +1,9 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import { Provider } from "react-redux";
-import { store, persistor } from "../redux/store.js";
-import { PersistGate } from "redux-persist/integration/react";
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App/App';
+import { Provider } from 'react-redux';
+import { store, persistor } from '../redux/store.js';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const Root = () => (
   <Provider store={store}>
